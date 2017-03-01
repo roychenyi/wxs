a71e7289441ced3d32ce324207d99889a3d4a5fd
陈义 <chenyi@coamc.com>
2017-03-01 11:51:31 +0800

    config update

--- wxs-dao/src/test/java/com/cx/wxs/dao/AppTest.java
+++ wxs-dao/src/test/java/com/cx/wxs/dao/AppTest.java
@@ -1,38 +1,11 @@
 package com.cx.wxs.dao;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.*;
+public class AppTest {
+	
+	@Test
+	public  void test(){
+		System.out.println("hello test！");
+	}
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
